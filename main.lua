0cd1ecadeda53ba5560cb8d957cd2c00c90cf546
Paulyn <realjakeojeff@gmail.com>
2026-07-22 12:46:11 +0530

    Setting a double matrix grid

--- main.lua
+++ main.lua
@@ -5,13 +5,33 @@ function love.load()
     for i = 1, 30 do
         createParticle(i * math.floor(math.randomseed(1,4)), i * math.floor(math.randomseed(1,4)), "right", "bottom")
     end
+
+    wW = 500
+    wH = 500
+
+    grids = {}
+    rows = 250
+    size = wW/rows
+    cols = wH/size
+
+
+    for i = 1, rows do
+        grids[i] = {}
+        for j = 1, cols do
+            grids[i][j] = {
+                x = (i - 1) * size,
+                y = (j - 1) * size,
+                
+            }
+        end
+    end
+
+
 end
 
 function love.update(dt)
     for i, v in ipairs(particles) do
-        if v.type == "ants" do
-            
-        end
+
     end
 end
 
@@ -25,8 +45,8 @@ function createParticle(gX, gY, xDir, yDir)
             gX = gX or 0,
             gY = gY or 0,
             color = {1,1,1},
-            xImpDir = "right",
-            yImpDir = "bottom",
+            xImpDir = xDir or "right",
+            yImpDir = yDir or "bottom",
             acceleration = 1,
             type = "ants"
         }
