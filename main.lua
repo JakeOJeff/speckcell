589c754bfd237889664472d456e0c2412fe9848c
Paulyn <realjakeojeff@gmail.com>
2026-07-24 22:14:06 +0530

    Drawing particles

--- main.lua
+++ main.lua
@@ -1,10 +1,7 @@
 
 
 function love.load()
-    particles = {}
-    for i = 1, 30 do
-        createParticle(i * math.floor(math.randomseed(1,4)), i * math.floor(math.randomseed(1,4)), "right", "bottom")
-    end
+
 
     wW = 500
     wH = 500
@@ -21,27 +18,39 @@ function love.load()
             grids[i][j] = {
                 x = (i - 1) * size,
                 y = (j - 1) * size,
-                
+                holding = nil
+
             }
         end
     end
 
+    love.window.setMode(wW, wH)
 
+    particles = {}
+    for i = 1, 30 do
+        posX = i * (love.math.random(1,4))
+        posY = i * (love.math.random(1,4))
+        local particle = createParticle(posX, posY, "right", "bottom")
+        grids[posX][posY].holding = particle
+    end
 end
 
 function love.update(dt)
     for i, v in ipairs(particles) do
-
+        if v.xImpDir == "right" then
+            v.gX = v.gX + 1 * dt
+        end
     end
 end
 
 function love.draw()
-    
+    for i, v in ipairs(particles) do
+        love.graphics.rectangle("fill", v.gX * size, v.gY * size, size, size)
+    end
 end
 
 function createParticle(gX, gY, xDir, yDir)
-    table.insert(particles,
-        {
+    particle =         {
             gX = gX or 0,
             gY = gY or 0,
             color = {1,1,1},
@@ -50,5 +59,8 @@ function createParticle(gX, gY, xDir, yDir)
             acceleration = 1,
             type = "ants"
         }
+    table.insert(particles,
+        particle
     )
+    return particle
 end
