d5d43a117e8f592d6b4e4dbe7b4210ed48dbdb31
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-07-28 05:20:51 +0530

    Creating particle properties and metatables

--- particle.lua
+++ particle.lua
@@ -1,16 +1,35 @@
 local particle = {}
+particle.__index = particle
 
+function particle:new(type, gX, gY, xDir, yDir, color, movementType, onCollide, targetType)
+    local self = setmetatable({}, particle)
 
-function particle:new()
+    self.gX, self.gY = gX or 1, gY or 1
+    self.pX, self.pY = gX or 1, gY or 1
     
+    self.color = color or { 1, 1, 1 }
+    xImpDir = xDir or 1
+    yImpDir = yDir or 1
+    acceleration = 100
+    type = type or "life"
+    collidable = true
+    target = nil
+    items = {}         -- items the creatures carry or have consumed
+    locations = {}     -- Locations the creatures or whatever remembers
+
+    movementType = movementType
+    onCollide = onCollide
+    targetType = targetType
+    table.insert(particles, particle)
+    return particle
 end
 
 function particle:update(dt)
-    
+
 end
 
 function particle:draw()
-    
+
 end
 
-return
+return particle
