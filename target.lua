d9e4f2914b1d673595c631d45cd184c552a26e0b
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-07-29 02:03:54 +0530

    Modularizing the target systems

--- target.lua
+++ target.lua
@@ -1,12 +1,20 @@
-function findUninfected(v, type)
-    local targetX, targetY = v.pX, v.pY         -- default: don't move if no target
-    if v.type == "infected" then
-        local target = nearestParticle(v, type)
+function particle:findUninfectedTarget(type)
+    local targetX, targetY = self.pX, self.pY         -- default: don't move if no target
+    if self.type == "infected" then
+        local target = nearestParticle(self, type)
         if target then targetX, targetY = target.pX, target.pY end
     end
     return targetX, targetY
 end
 
-function findParticle(v, type)
+function particle:findParticleRadius(targetCenter)
     
+    local targetX, targetY = self.pX, self.pY
+    local radius = 35
+    if self.type == "life" then
+        if targetCenter then
+            targetX, targetY = love.math.random(self.x - radius, self.x + radius), love.math.random(self.y - radius, self.y + radius)
+        end
+    end
+    return targetX, targetY
 end
