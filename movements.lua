216e5cd4f5304398ce68b609fb05f107c0cff29b
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-07-28 16:28:37 +0530

    Modularizing movement for particle

--- movements.lua
+++ movements.lua
@@ -1,17 +1,17 @@
-function randomMovement(v, targetX, targetY)
+function particle:randomMovement(targetX, targetY)
     return love.math.random() * 2 - 1, love.math.random() * 2 - 1
 end
 
-function targetedMovement(v, targetX, targetY)
+function particle:targetedMovement(targetX, targetY)
     local impX, impY
 
-    if targetX - v.pX > 0 then
+    if targetX - self.pX > 0 then
         impX = 1
     else
         impX = -1
     end
 
-    if targetY - v.pY > 0 then
+    if targetY - self.pY > 0 then
         impY = 1
     else
         impY = -1
